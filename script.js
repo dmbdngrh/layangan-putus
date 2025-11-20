4a7520ce5379627b1cf388a7ac967fdf94d32adf
dmbdngrh <dimasbudin777@gmail.com>
2025-11-20 16:41:18 +0700

    add graphics

--- script.js
+++ script.js
@@ -27,16 +27,40 @@ class Kite {
     this.y = y;
     this.width = 48;
     this.height = 48;
-    this.color = "red";
 
-    window.addEventListener("keydown", (e) => (this.keys[e.key] = true));
-    window.addEventListener("keydown", (e) => (this.keys[e.key] = false));
+    this.speedX = 0;
+    this.speedY = 0;
+
+    this.keys = {};
+    this.pressTime = null;
+    this.isHeld = false;
+    this.launchTreshold = 0.5;
+
+    this.color = "rgba(255, 0, 0, 0.5)";
+    this.img = img;
+
+    window.addEventListener("keydown", (e) => {
+      this.speedX = 100;
+      this.speedY = -100;
+    });
+    window.addEventListener("keyup", (e) => {
+      this.speedX = 0;
+      this.speedY = 0;
+    });
   }
 
   update() {
     const context = gameArea.context;
+
     context.fillStyle = this.color;
-    context.fillRect(this.x, this.y, this.width, this.height);
+    context.drawImage(this.img, this.x - this.width / 2, this.y - this.height / 2, this.width * 2, this.height * 2);
+    // context.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  movement(deltaTime, time) {
+    // console.log(deltaTime);
+    this.x += this.speedX * deltaTime;
+    this.y += this.speedY * deltaTime;
   }
 
   handleInput() {}
@@ -55,25 +79,35 @@ const canvasHeight = 480;
 function startGame() {
   init();
   gameArea.start();
-  requestAnimationFrame(gameLoop);
 }
 
 function init() {
+  gameArea = new GameArea(canvasWidth, canvasHeight);
+  const kiteSprite = new Image();
+  kiteSprite.src = "img/ph-kite.png";
+  kiteSprite.onload = () => {
+    kite = new Kite((canvasWidth - 48) / 2, (canvasHeight - 48) / 2, kiteSprite);
+    requestAnimationFrame(gameLoop);
+  };
   previousTime = 0;
   score = 0;
-  gameArea = new GameArea(canvasWidth, canvasHeight);
-  kite = new Kite((canvasWidth - 48) / 2, (canvasHeight - 48) / 2);
+  obstacles = [];
 }
 
-function update(deltaTime) {
+function update(time) {
+  const deltaTime = (time - previousTime) / 1000;
+  previousTime = time;
+
+  kite.movement(deltaTime, time);
+}
+
+function render() {
   gameArea.clear();
   kite.update();
 }
 
 function gameLoop(time) {
-  const deltaTime = (time - previousTime) / 1000;
-  previousTime = time;
-  update(deltaTime);
-  //render;
+  update(time);
+  render();
   if (time < 10000) requestAnimationFrame(gameLoop);
 }
